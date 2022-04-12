ea01a4352b2772e0c88bd076e0cc46d33efb41ec
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-04-12 14:51:06 -0600

    Implemented logout feature that revokes JWT upon signout

--- FinalChatApp/ClientApp/src/App.js
+++ FinalChatApp/ClientApp/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import { ChatApp } from "./components/chat-app";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SignUpForm } from "./components/loginpage/signUpForm";
@@ -8,10 +9,23 @@ import Home from "./components/home";
 import Nav from "./components/Nav";
 
 function App() {
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://localhost:7089/api/user", {
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const content = await response.json();
+
+      setUsername(content.username);
+    })();
+  });
+
   return (
     <div className="App">
       <BrowserRouter history={history}>
-        <Nav />
+        <Nav username={username} />
         <header className="App-header">
           <Routes>
             <Route path="/" exact element={<Home />}></Route>
--- FinalChatApp/ClientApp/src/components/Nav.js
+++ FinalChatApp/ClientApp/src/components/Nav.js
@@ -1,7 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = (props) => {
+  const logout = async () => {
+    fetch("https://localhost:7089/api/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+  };
+
+  let navMenu;
+
+  if (props.username === "") {
+    navMenu = (
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <Link to="register" class="nav-link">
+            Register
+          </Link>
+        </li>
+        <li class="nav-item">
+          <Link to="login" class="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    navMenu = (
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">Hi, {props.username}</li>
+        <li class="nav-item">
+          <Link to="login" class="nav-link" onClick={logout}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" class="navbar-brand">
@@ -20,19 +58,7 @@ const Nav = () => {
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <Link to="register" class="nav-link">
-              Register
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link to="login" class="nav-link">
-              Sign in
-            </Link>
-          </li>
-          {/* <li class="nav-item">
-            <Link class="nav-link">Logout</Link>
-          </li> */}
+          <li class="nav-item">{navMenu}</li>
         </ul>
       </div>
     </nav>
